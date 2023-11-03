8c49f85d90a055d416cfa5f3e9a2b7d6ac1a499e
Ruchita495 <122808258+Ruchita495@users.noreply.github.com>
2023-11-03 11:49:52 -0400

    add Homeconroller and Home folder in Are->Customer folder

--- RuchitasBookStore/Areas/Customer/Controllers/HomeController.cs
+++ RuchitasBookStore/Areas/Customer/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 
 namespace RuchitasBookStore.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
