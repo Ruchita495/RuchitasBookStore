e4e168b97570c5ca6899d22db86f1065e717b351
Ruchita495 <122808258+Ruchita495@users.noreply.github.com>
2023-11-05 13:50:41 -0500

    add additional code in IRepository.cs file

--- RuchitasBooks.DataAccess/Repository/IRepository/IRepository.cs
+++ RuchitasBooks.DataAccess/Repository/IRepository/IRepository.cs
@@ -9,11 +9,23 @@ namespace RuchitasBooks.DataAccess.Repository.IRepository
 {
     public interface IRepository<T> where T : class
     {
-        T Get(int id);
+        T Get(int id); // Retrive a category from the database by id
+
+        // List of categories based on requirements
         IEnumerable<T> GetAll(
             Expression<Func<T, bool>> filter = null,
             Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
                 string includeProperties = null
     );
+
+        T GetFirstOrDefault(
+            Expression<Func<T, bool>> filter = null,
+            string includeProperties = null
+            );
+
+        void Add(T entity); // to add an entity
+        void Remove(int id); // to remove object or category
+        void Remove(T entity); // anpother way to remove an object
+        void RemoveRange(IEnumerable<T> entity); // removes a complete range of entitie
     }
 }
